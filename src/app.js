21078ca9fd56a59caa3d6ad184ce8c1549d80b6e
thiepn <junsosmart@gmail.com>
2026-09-23 16:24:15 +0200

    Mark portable metadata as Implementation Phase 16

--- src/app.js
+++ src/app.js
@@ -3429,7 +3429,7 @@ function downloadCustomExperience(experience) {
 }
 
 
-const PORTABILITY_APP_VERSION = "implementation-15";
+const PORTABILITY_APP_VERSION = "implementation-16";
 
 function downloadTextFile(text, filename, type = "application/json") {
   const blob = new Blob([text], { type });
